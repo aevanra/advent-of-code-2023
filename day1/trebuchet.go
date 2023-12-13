116594fdd1cb4a9dd59c55c1864589c544bb53cd
aevanra <aevan.armus@gmail.com>
2023-12-12 23:40:13 -0500

    day 1 in progress -- need to maybe fix some regex?

--- day1/trebuchet.go
+++ day1/trebuchet.go
@@ -1,26 +1,30 @@
 package trebuchet
 
 import (
+    "fmt"
     "strconv"
+    "regexp"
 )
 
-func Trebuchet(document []string) int {
+func TrebuchetPart1(document []string) int {
     response := 0
     results := []int{}
+
     for _, v := range(document) {
-        digits := make([]int, 2)   
+        digits := make([]int, 0)   
 
         for _, c := range(v) {
-            if digit, err := strconv.Atoi(string(c)); err != nil {
-               digits = append(digits, digit) 
+            if digit, err := strconv.Atoi(string(c)); err == nil {
+                
+                digits = append(digits, digit) 
             }
         }
-
+        
         if len(digits) == 1 {
-            digits[1] = digits[0]
+            digits = append(digits, digits[0])
         }
 
-        results = append(results, sliceToInt(digits))
+        results = append(results, getNum(digits))
     }
 
     for _, i := range(results) {
@@ -30,7 +34,12 @@ func Trebuchet(document []string) int {
     return response
 }
 
-func sliceToInt(s []int) int {
+func getNum(s []int) int {
+    if len(s) > 1 {
+        s = []int{s[0], s[len(s)-1]} 
+    } else {
+        s = []int{s[0],s[0]}
+    }
     res := 0
     op := 1
 
@@ -40,4 +49,76 @@ func sliceToInt(s []int) int {
     }
 
     return res
+}
+
+// Can vastly improve the above by having a pointer at the beginning and a pointer at the end of every string and only 
+// searching until two numbers are found instead of finding each one -- may implement that may not.
+
+
+func TrebuchetPart2(document []string) int {
+    res := 0
+    result := []int{}
+    stringToIntConversions := map[string]int{
+        "one": 1,
+        "two": 2,
+        "three": 3, 
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
+    }
+    re := regexp.MustCompile(`\d|one|two|three|four|five|six|seven|eight|nine`)
+
+    for _, v := range(document) {
+        digits := re.FindAllString(v, -1)
+        digits_int := make([]int, 0)
+
+        if digits == nil {
+            continue
+        } else if len(digits) == 1 {
+            digits = append(digits, digits[0])
+            fmt.Println(digits)
+        } else {
+            digits = []string{digits[0], digits[len(digits)-1]}
+        }
+
+        if len(digits) > 2 {
+            fmt.Println(digits)
+        }
+
+        for i, match := range(digits) {
+
+            if i > 1 {
+                fmt.Println(i)
+            }
+
+            if len(match) > 2 {
+                
+                if stringToIntConversions[match] > 9 {
+                    fmt.Println(stringToIntConversions[match])
+                }
+
+                digits_int = append(digits_int, stringToIntConversions[match])
+            } else {
+                d, err := strconv.Atoi(digits[i])
+
+                if err != nil {
+                    panic("We can't count!!!")
+                }
+
+                digits_int = append(digits_int, d)
+            }
+        }
+
+        result = append(result, getNum(digits_int))
+    }
+
+    for _, i := range(result) {
+        res += i 
+    }
+
+    return res
+
 }
--- main.go
+++ main.go
@@ -2,13 +2,31 @@ package main
 
 import (
     "fmt"
+    "github.com/aevanra/advent-of-code-2023/day1"
+    "bufio"
+    "os"
 )
 
 func main() {
-    trebuchet_prompt := `1abc2
-pqr3stu8vwx
-a1b2c3d4e5f
-treb7uchet`
+    day1Input, err := os.Open("day1/input.txt")
 
+    if err != nil {
+        panic("We cannot read!!!")
+    }
+
+    scanner := bufio.NewScanner(day1Input)
+    scanner.Split(bufio.ScanLines)
+
+    trebuchet_input := make([]string, 0)
+
+    for scanner.Scan() {
+        trebuchet_input = append(trebuchet_input, scanner.Text())
+    }
+
+    fmt.Println("Trebuchet Problem Part 1")
+    fmt.Println(trebuchet.TrebuchetPart1(trebuchet_input))
+    
+    fmt.Println("\nTrebuchet Problem Part 2")
+    fmt.Println(trebuchet.TrebuchetPart2(trebuchet_input))
     
 }
